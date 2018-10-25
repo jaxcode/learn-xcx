021d647d66a84cf5ce56a2063526a35ab3d5ad1e
Jax <mini123456@qq.com>
2018-10-25 16:41:28 +0800

    添加 like 组件

---
+++ components/like/index.js
@@ -0,0 +1,29 @@
+// components/like/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    count1: 99,
+    count2: 998,
+    like: true,
+    yesSrc: 'images/like.png',
+    noSrc: 'images/like@dis.png'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onLink (event) {
+      this.setData({like: !this.data.like})
+    }
+  }
+})
